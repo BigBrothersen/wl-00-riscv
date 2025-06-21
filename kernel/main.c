ef82b99493186150cd3cfd3e29a421c8b58d4f33
William Hansen Loe <148175814+BigBrothersen@users.noreply.github.com>
2025-06-21 14:47:55 +0800

    Add files via upload
    Add booting and debug functions.

---
+++ kernel/main.c
@@ -0,0 +1,13 @@
+#include "print.h"
+
+void main() {
+    // int val = -55;
+    // print_string("Reaching main now");
+    // printf("%s", "Checking if these work\n");
+    // printf("The value of this number is %d\n", val);
+    // printf("%d + %d = %d", 10, 12, 10+12);
+    // printf("End of Line\n");
+    printf("wl-00 kernel is booting");
+    printf("\n");
+    while(1);
+}
